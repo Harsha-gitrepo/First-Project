52a08562b517b3be641414c953926fbd3a69e517
Harsha-gitrepo <harsha.itcareer9@gmail.com>
2024-11-11 10:47:05 +0530

    Third Commit

--- src/main/java/patternPrograms/basicPattern.java
+++ src/main/java/patternPrograms/basicPattern.java
@@ -11,24 +11,11 @@ public class basicPattern {
         }
     }
 
-    public void leftTriangleStarPattern(){
-        int i,j, row=6;
-        for ( i = 0; i < row; i++) {
-            for ( j = (row-i); j >= 0; j--) {
-                System.out.print(" ");
-            }
-            for (j=0;j<=i;j++)
-            {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
+
     public static void main(String[] args) {
         basicPattern obj = new basicPattern();
         System.out.println("rightTriangleStarPattern");
         obj.rightTriangleStarPattern();
-        System.out.println("leftTriangleStarPattern");
-        obj.leftTriangleStarPattern();
+
     }
 }
--- src/main/java/patternPrograms/pyramidPattern.java
+++ src/main/java/patternPrograms/pyramidPattern.java
@@ -1,19 +1,24 @@
 package patternPrograms;
 
 public class pyramidPattern {
-    public static void main(String[] args) {
-        for (int i=5; i>0; i++) {
 
-          for (int j=0; j<i; j++)
-          {
-              System.out.print(" ");
-              for (int k=1; k<=0;j++)
-              {
-                  System.out.print("*");
-              }
-
-          }
-            System.out.println("");
+    public void leftTriangleStarPattern(){
+        int i,j, row=6;
+        for ( i = 0; i < row; i++) {
+            for ( j = (row-i); j >= 0; j--) {
+                System.out.print(" ");
+            }
+            for (j=0;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
         }
+    }
+
+    public static void main(String[] args) {
+        pyramidPattern obj = new pyramidPattern();
+        System.out.println("leftTriangleStarPattern");
+        obj.leftTriangleStarPattern();
     }
 }
